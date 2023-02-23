efa23168d17b4e950fbd36526b2b087e7e138907
backli1ght <1124887276@qq.com>
2023-02-23 20:10:51 +0800

    master

--- test/binarySearch.java
+++ test/binarySearch.java
@@ -2,4 +2,7 @@ public class binarySearch {
     public static void main(String[] args) {
         System.out.println("this is binary search");
     }
+    public void dfs(){
+        System.out.println("this is dfs");
+    }
 }
